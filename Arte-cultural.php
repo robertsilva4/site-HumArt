9a487dbf204d8e4877ca44a64278ea4f13ae4552
Robert Silva <robert.silva4@fatec.sp.gov.br>
2019-11-28 16:28:10 -0300

    atualizado 2

--- Arte-cultural.php
+++ Arte-cultural.php
@@ -60,7 +60,7 @@
     </div>
     <div class=" align-text-top bg-dark">
         <footer>
-            <?php include 'php/footer.php';?>
+            <?php include 'php/footer.php' ; ?>
         </footer>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
