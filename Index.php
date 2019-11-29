bf7718bef97cbaeadc6d3f09cc767aad4acfc677
Robert Silva <robert.silva4@fatec.sp.gov.br>
2019-11-29 18:34:44 -0300

    version 3.1

--- Index.php
+++ Index.php
@@ -7,12 +7,20 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="bootstrap-4.3.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/estilo.css" />
+  <link rel="stylesheet" href="css/estilo.css" >
 
   <title>HumArt</title>
 </head>
 
 <body>
+</div>
+  <div class="text-center p-lg-4 col-lg-5 float-right alert-danger alert-dismissible fade show" role="alert">
+  <strong>Atenção !</strong> 
+  <p>Este site utiliza cookies, para ver as normas de utilização do site <a href="#">clique aqui</a></p>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+  </div>
   <div class="container">
     <div class="row">
       <header>
@@ -64,7 +72,7 @@
           </div>
           <div class="col-lg-4 float-lg-right">
             <aside>
-              <h2 class="text-center p-3">texto home</h2>
+              <h2 class="text-center p-3">O que é arte? </h2>
               <p class="text-justify ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu pharetra arcu. Nunc bibendum dui et gravida viverra. Morbi pretium cursus purus, maximus ornare sem tempor et. Nulla vitae eros lobortis, laoreet mi vitae, consequat turpis. Fusce enim diam, fringilla nec ornare ut, laoreet sit amet diam. Nunc vel purus ac quam commodo porta. Praesent vitae massa nibh. Aliquam laoreet fermentum diam vitae lobortis. Morbi consectetur arcu et augue vestibulum, eu mattis quam dignissim. Nullam semper lobortis orci, et ornare ligula mattis at.</p>
             </aside>
           </div>
@@ -76,7 +84,6 @@
     <footer>
       <?php include 'php/footer.php';?>
     </footer>
-  </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
