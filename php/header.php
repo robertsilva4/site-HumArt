e6e1bfaa483181635ba4eacb1eba36413a4be948
Robert Silva <robert.silva4@fatec.sp.gov.br>
2019-12-02 20:26:08 -0300

    atualizado

--- php/header.php
+++ php/header.php
@@ -2,7 +2,7 @@
 
 echo'<div class="col-lg-12">
 <a href="Index.php"> 
-<img src="images/Logo%20HumArt_200x375.jpg" class=" float-left rounded float-left" 
+<img src="images/Logo HumArt_200x375.jpg" class=" float-left rounded float-left" 
 alt="Logo da empresa HumArt com duas pessoas de diferentes épocas escrevendo na parede." height="200" width="375"> 
 </a>
 </div>';
