a61e55576babdee4a9f08552174a79da5bbe95f7
Robert Silva <robert.silva4@fatec.sp.gov.br>
2019-12-03 09:25:01 -0300

    version 3.6

--- Contatos.php
+++ Contatos.php
@@ -61,7 +61,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="TituloModalCentralizado">Título do modal</h5>
+        <h5 class="modal-title" id="TituloModalCentralizado">Sucesso</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
           <span aria-hidden="true">&times;</span>
         </button>
