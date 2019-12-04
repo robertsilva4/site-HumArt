43bf5f048f4c717d17b6780d1a9f9fbe79da8ad7
Robert Silva <robert.silva4@fatec.sp.gov.br>
2019-12-03 22:15:00 -0300

    atualizado

--- Especiesdaarte.php
+++ Especiesdaarte.php
@@ -3,48 +3,100 @@
 <html lang="pt-BR">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="bootstrap-4.3.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/estilo.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap-4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css" />
 
-  <title>HumArt</title>
+    <title>HumArt</title>
 </head>
 
 <body>
-  <div class="container">
-    <div class="row">
-      <header>
-        <?php include 'php/header.php';?>
-      </header>
+    <div class="container">
+        <div class="row">
+            <header>
+                <?php include 'php/header.php';?>
+            </header>
+        </div>
+        <div class="row">
+            <?php include 'php/nav.php';?>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <main>
+                    <div class="col-lg-7 float-lg-left">
+                        <article>
+                            <h2 class="text-center">O que é Arte</h2>
+                            <p class="text-center">O que é História da Arte?</p>
+                            <p class="text-justify">A história da arte é um área do conhecimento que analisa os estilos artísticos, suas modificações, obras de arte, artistas e o valor estético das obras produzidas. Esse estudo é realizado de acordo com o cenário social/político/religioso que a sociedade viveu ou vive em determinado período. Outras ciências auxiliam nessas análises tais como a arqueologia, história, paleografia, filosofia, sociologia, etc.</p>
+
+                            <p class="text-justify">É uma disciplina que está no plano de aula de alguns cursos superiores e também como curso de graduação para aqueles que desejam se formar na área e exercer profissão de curador, crítico de arte (realiza uma análise sobre as obras, artistas ou expositores) ou gestor de museus, galerias, centros culturais e escolas de arte.</p>
+                            <img src="images/otzi.jpg" alt="imagem otzi" width="250" height="250">
+                            <p class="text-justify">
+                                Os primeiros registros da Tatuagem
+                                O registro mais antigo de uma tatuagem foi descoberto em 1991 no cadáver congelado de um homem da Idade do Cobre. Os restos mortais do homem, que foi apelidado pelos cientistas de “Ötzi”, datam de 3.300 anos antes de Cristo. Em seu corpo foram encontradas diversas linhas na região das costas, tornozelos, punhos, joelhos e pés. Supõe-se que os desenhos tenham sido criados a partir da fricção de carvão em cortes verticais feitos na pele.</p>
+
+                            <p class="text-justify"> https://www.megacurioso.com.br/tatuagens/37264-voce-sabe-quais-sao-as-origens-da-tatuagem-.htm apud: Fonte da imagem: Reprodução/IDW</p>
+
+                            <p class="text-justify">Depois de estudar o corpo, exames de raio X revelaram degenerações ósseas ao lado de cada uma das tatuagens. Isso levou os cientistas a acreditar que o povo de Ötzi – que são os ancestrais de parte dos europeus – utilizasse os desenhos como uma espécie de tratamento médico para diminuir a dor.</p>
+                            <p class="text-justify">(fonte: https://www.megacurioso.com.br/tatuagens/37264-voce-sabe-quais-sao-as-origens-da-tatuagem-.htm) </p>
+                                <img src="images/rupestre.jpg" src="imagem de rupestre" height="250" width="250"> 
+                            <p class="text-justify">As primeiras aparições da arte Tribal: são impossivéis de mensurar o inicio, pois datam das primeiras formas organizacionais da soiciedes, logo se considerando suas apariçoes com os perios pré hitoricos e as pinturas rupestres e as marcações de clãs e tribos, assim que se descobriram as primeiras formas de paramentaçoes e idumentarias, como cocares, pinturas coporais e pigmentaçoes.</p>
+
+                            <p class="text-justify">(fonte: https://conhecimentocientifico.r7.com/arte-rupestre-o-que-e-quando-onde-e-por-quem-foi-feita/ )</p>
+
+                            <p class="text-justify">Arte rupestre: o que é, quando, onde e por quem foi feita?(24/02/2019, 07h00)</p>
+                            <p class="text-justify">A arte rupestre ainda é um mistério para a humanidade, porém causa admiração e também nos conta como era o cotidiano no mundo pré-histórico.</p>
+                            <p class="text-justify">A arte rupestre são as criações artísticas da Pré-História e possivelmente datadas de 40.000 a.C. É também denominada pintura ou gravura rupestre. Mas não é possível precisar a idade das pinturas, posto que a datação por carbono pode conter erros.</p>
+
+                            <p class="text-justify">Pode-se encontrar a arte rupestre em todos os continentes da Terra, certamente que feita na Era Paleolítica. Inclusive no Brasil há numerosos sítios arqueológicos preocisos e bem conservados.</p>
+                            <p class="text-justify">(fonte: https://conhecimentocientifico.r7.com/arte-rupestre-o-que-e-quando-onde-e-por-quem-foi-feita/ )</p>
+                        </article>
+                        </div>
+                        <div class="col-lg-5 float-right">
+                        <aside>
+                            <p class="">Arte & Cultura</p>
+
+                            <p class="text-justify">No tocante a Arte das tradições Culturais, pode-se associar ao principio das organizações modernas da sociedade, ou o reconhecimento das nações e das patrias, tais quais ciraram de modo ou de outro, por suas formações ideologicas e sociais de forma religiosa ou positivistas, as fundamentações de ritos, rituais ( não nescessariamente religiosos, por exemplo se considerando as tradições e os costumes;), desta forma a organização e a criação de culturas dos paises, geraram valores e estes se refletem nas suas formas de expressões culturais, algumas mais comuns e outras nem tanto salientando sempre que isso é que tornam suas identidades artisticas como unicas e por vezes alternativas ou seculares.</p>
+
+                            <p class="text-justify">(fonte: https://www.todamateria.com.br/cultura-africana/) </p>
+                            <img src="images/culturaafricanadanca-cke.jpg" alt="imagem da cultura africana" width="250" height="250">
+                            <p class="text-justify">Cultura Africana</p>
+                            <p class="text-justify">A cultura africana deve ser observada sempre no plural, haja vista sua existência milenar e sua vasta diversidade. Cumpre lembrar que a África não é um país.</p>
+
+                            <p class="text-justify">A arqueologia aponta a África como o território habitado há mais tempo no planeta. Isso resultou na profusão de idiomas com mais de mil línguas, religiões, regimes políticos, condições materiais de habitação e atividades econômicas.</p>
+
+                            <p class="text-justify">Atualmente, o continente africano ocupa um quinto da Terra, com mais de 50 países e quase 1 bilhão de habitantes.</p>
+
+                            <p class="text-justify">Etnocentrismo, Eurocentrismo e Culturas Africanas</p>
+                            <p class="text-justify">É fato conhecido que a história africana foi escrita e contada pelos colonizadores europeus.</p>
+
+                            <p class="text-justify">Os viajantes, missionários e dirigentes coloniais foram os responsáveis pelos primeiros relatos acerca da cultura dos povos africanos.</p>
+
+                            <p class="text-justify">Assim, além de serem capturados para alimentarem a escravidão colonial, estes povos foram usurpados em todos os seus direitos, incluindo o de contar a própria história.</p>
+
+                            <p class="text-justify">O “Etnocentrismo” e o “Eurocentrismo” nas ciências europeias durante o século XIX são os responsáveis pela concepção das culturas africanas.</p>
+
+                            <p class="text-justify">Nesse viés, elas são consideradas manifestações primitivas ou bárbaras, típicas dos primeiros estágios da civilização.</p>
+
+                            <p class="text-justify">Hoje, com as independências dos países africanos, há um esforço de recuperação das tradições culturais africanas, bem como a constituição de uma historiografia local.</p>
+                            <p class="text-justify">Juliana Bezerra</p>
+                            <p class="text-justify">Professora de História</p>
+                        </aside>
+                    </div>
+                </main>
+            </div>
+        </div>
     </div>
-    <div class="row">
-      <?php include 'php/nav.php';?>
+    <div class=" align-text-top bg-dark">
+        <footer>
+            <?php include 'php/footer.php';?>
+        </footer>
     </div>
-    <div class="row">
-      <div class="col-lg-12">
-        <main>
-          <div class="col-lg-7 float-lg-left">
-            <h1>Especies da Arte</h1>
-          </div>
-          <div class="col-lg-5 float-lg-right">
-            <aside>
-              <h2>texto</h2>
-            </aside>
-          </div>
-        </main>
-      </div>
-    </div>
-  </div>
-  <div class=" align-text-top bg-dark">
-    <footer>
-      <?php include 'php/footer.php';?>
-    </footer>
-  </div>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- site Humart/php/header.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo'<div class="col-lg-12">
-<a href="Index.php"> 
-<img src="images/Logo_HumArt_200x375.jpg" class="float-left" 
-alt="Logo da empresa HumArt com duas pessoas de diferentes épocas escrevendo na parede." height="200" width="375"> 
-</a>
-</div>';
