94ac69af6077680e0b9763dc69325b3ed2bbdbf9
dmykhaos <55214518+dmykhaos@users.noreply.github.com>
2019-12-03 21:04:45 -0300

    Add files via upload

--- Especiesdaarte.php
+++ Especiesdaarte.php
@@ -18,6 +18,89 @@
       <header>
         <?php include 'php/header.php';?>
       </header>
+
+      <article>
+      <h2>O que é Arte</h2>
+<p>O que é História da Arte?</p>
+<p>A história da arte é um área do conhecimento que analisa os estilos artísticos, suas modificações, obras de arte, artistas e o valor estético das obras produzidas. Esse estudo é realizado de acordo com o cenário social/político/religioso que a sociedade viveu ou vive em determinado período. Outras ciências auxiliam nessas análises tais como a arqueologia, história, paleografia, filosofia, sociologia, etc.</p>
+
+<p>É uma disciplina que está no plano de aula de alguns cursos superiores e também como curso de graduação para aqueles que desejam se formar na área e exercer profissão de curador, crítico de arte (realiza uma análise sobre as obras, artistas ou expositores) ou gestor de museus, galerias, centros culturais e escolas de arte.</p>
+
+<p>Tipos de Artes</p>
+<p>De acordo com o segmento, a arte pode ser classificada de várias maneiras: artes plásticas, artes cênicas, artes visuais, etc. Além disso, existem graduações específicas para que uma pessoa se especialize na área de sua escolha. Veja a definição de algumas:</p>
+
+<p>Artes Plásticas:está relacionada a escultura, arquitetura, artes gráficas e o artesanato.</p>
+
+<p>Artes Visuais: é a classificação dada para todos os tipos de arte que retratam a realidade ou a imaginação e que tem a visão como um dos principais recursos para estudo. Envolve áreas como a pintura, cinema, decoração, jogos, etc.</p>
+
+<p>Artes Cênicas: é o estudo de todas as formas de expressão realizadas através da dança, do teatro ou da música.</p>
+
+<p>Segundo autores como Hegel e Ricciotto Canudo (que considerou o cinema como 7ª arte através do Manifesto das Sete Artes e Estética da Sétima Arte, em 1912), teóricos e críticos de arte, há uma lista numerada do que pode ser considerado arte nos dias atuais, principalmente com o advento da tecnologia:</p>
+
+<p>Música: é um tipo de arte que se baseia em sons e ritmos de acordo com determinado período de tempo;</p>
+<p>Dança/Coreografia: a dança está classificada dentro das artes cênicas, e é uma forma de movimento que se realiza com o corpo baseado ou não em uma coreografia (arte de criar roteiros/trilhas de movimentos para realizar uma dança);</p>
+<p>Pintura: está relacionada a cor e suas variações, bem como a forma com que o artísta a utiliza em uma superfície;
+<p>Escultura: é uma forma de arte em que há a criação de imagens plásticas em relevo utilizando vários tipos de materiais (bronze, mármore, argila, madeira, etc.);</p>
+<p>Teatro: é um tipo de arte em que um ou mais atores encenam uma determinada história ou situação em local específico (anfiteatros, praças, ruas, etc.);</p>
+<p>Literatura: é uma arte que utiliza a palavra para criação de histórias ou poesias de acordo com técnicas específicas;</p>
+<p>Cinema: é uma arte e técnica criada para a reprodução de imagens com movimento em uma tela;</p>
+<p>Fotografia: se baseia em imagens e técnicas para capturar paisagens e seus diversos momentos;
+<p>Histórias em Quadrinhos: forma de arte que utiliza a cor, a palavra e imagem para narrar uma história;</p>
+<p>Jogos de Computador e de Vídeo: constitui na criação de jogos que podem ser reproduzidos por meio de uma aparelho eletrônico com imagens, cores e sons que fazem com que o jogador interaja com ele;</p>
+<p>Arte digital: é a arte produzida por meio de programas de computador relacionados às artes gráficas, que possibilitam criações em 3D e 2D.</p>
+<p>(fonte: http://historia-da-arte.info/o-que-e-arte.html) </p>
+
+
+
+<p>Os primeiros registros da Tatuagem
+O registro mais antigo de uma tatuagem foi descoberto em 1991 no cadáver congelado de um homem da Idade do Cobre. Os restos mortais do homem, que foi apelidado pelos cientistas de “Ötzi”, datam de 3.300 anos antes de Cristo. Em seu corpo foram encontradas diversas linhas na região das costas, tornozelos, punhos, joelhos e pés. Supõe-se que os desenhos tenham sido criados a partir da fricção de carvão em cortes verticais feitos na pele.</p>
+ 
+<p>  https://www.megacurioso.com.br/tatuagens/37264-voce-sabe-quais-sao-as-origens-da-tatuagem-.htm apud: Fonte da imagem: Reprodução/IDW</p>
+
+<p>Depois de estudar o corpo, exames de raio X revelaram degenerações ósseas ao lado de cada uma das tatuagens. Isso levou os cientistas a acreditar que o povo de Ötzi – que são os ancestrais de parte dos europeus – utilizasse os desenhos como uma espécie de tratamento médico para diminuir a dor.</p>
+<p>(fonte: https://www.megacurioso.com.br/tatuagens/37264-voce-sabe-quais-sao-as-origens-da-tatuagem-.htm) </p>
+
+<p>As primeiras aparições da arte Tribal: são impossivéis de mensurar o inicio, pois datam das primeiras formas organizacionais da soiciedes, logo se considerando suas apariçoes com os perios pré hitoricos e as pinturas rupestres e as marcações de clãs e tribos, assim que se descobriram as primeiras formas de paramentaçoes e idumentarias, como cocares, pinturas coporais e pigmentaçoes.</p>
+ 
+<p>(fonte: https://conhecimentocientifico.r7.com/arte-rupestre-o-que-e-quando-onde-e-por-quem-foi-feita/ )</p>
+
+<p>Arte rupestre: o que é, quando, onde e por quem foi feita?(24/02/2019, 07h00)</p>
+<p>A arte rupestre ainda é um mistério para a humanidade, porém causa admiração e também nos conta como era o cotidiano no mundo pré-histórico.
+<p>A arte rupestre são as criações artísticas da Pré-História e possivelmente datadas de 40.000 a.C. É também denominada pintura ou gravura rupestre. Mas não é possível precisar a idade das pinturas, posto que a datação por carbono pode conter erros.</p>
+
+<p>Pode-se encontrar a arte rupestre em todos os continentes da Terra, certamente que feita na Era Paleolítica. Inclusive no Brasil há numerosos sítios arqueológicos preocisos e bem conservados.
+<p>(fonte: https://conhecimentocientifico.r7.com/arte-rupestre-o-que-e-quando-onde-e-por-quem-foi-feita/ )</p>
+
+<p>Arte & Cultura</p>
+
+<p>No tocante a Arte das tradições Culturais, pode-se associar ao principio das organizações modernas da sociedade, ou o reconhecimento das nações e das patrias, tais quais ciraram de modo ou de outro, por suas formações ideologicas e sociais de forma religiosa ou positivistas, as fundamentações de ritos, rituais ( não nescessariamente religiosos, por exemplo se considerando as tradições e os costumes;), desta forma a organização e a criação de culturas dos paises, geraram valores e estes se refletem nas suas formas de expressões culturais, algumas mais comuns e outras nem tanto salientando sempre que isso é que tornam suas identidades artisticas como unicas e por vezes alternativas ou seculares.</p>
+ 
+<p>(fonte: https://www.todamateria.com.br/cultura-africana/) </p>
+<p>Cultura Africana</p>
+<p>A cultura africana deve ser observada sempre no plural, haja vista sua existência milenar e sua vasta diversidade. Cumpre lembrar que a África não é um país.</p>
+
+<p>A arqueologia aponta a África como o território habitado há mais tempo no planeta. Isso resultou na profusão de idiomas com mais de mil línguas, religiões, regimes políticos, condições materiais de habitação e atividades econômicas.</p>
+
+<p>Atualmente, o continente africano ocupa um quinto da Terra, com mais de 50 países e quase 1 bilhão de habitantes.</p>
+
+<p>Etnocentrismo, Eurocentrismo e Culturas Africanas</p>
+<p>É fato conhecido que a história africana foi escrita e contada pelos colonizadores europeus.</p>
+
+<p>Os viajantes, missionários e dirigentes coloniais foram os responsáveis pelos primeiros relatos acerca da cultura dos povos africanos.
+
+<p>Assim, além de serem capturados para alimentarem a escravidão colonial, estes povos foram usurpados em todos os seus direitos, incluindo o de contar a própria história.</p>
+
+<p>O “Etnocentrismo” e o “Eurocentrismo” nas ciências europeias durante o século XIX são os responsáveis pela concepção das culturas africanas.</p>
+
+<p>Nesse viés, elas são consideradas manifestações primitivas ou bárbaras, típicas dos primeiros estágios da civilização.</p>
+
+<p>Hoje, com as independências dos países africanos, há um esforço de recuperação das tradições culturais africanas, bem como a constituição de uma historiografia local.</p>
+<p>Juliana Bezerra</p>
+<p>Professora de História</p>
+
+<p>(fonte: https://www.todamateria.com.br/cultura-africana/) </p>
+
+      </article>
     </div>
     <div class="row">
       <?php include 'php/nav.php';?>
